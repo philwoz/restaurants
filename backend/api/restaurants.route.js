260d22a134f43ed2d7ffcfb19633069dd54c720d
philwoz <61164019+philwoz@users.noreply.github.com>
2022-01-05 16:07:32 +0000

    added reviews controller file + funcs

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,15 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviemsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+
+router
+    .route("/review")
+    .post(ReviemsCtrl.apiPostReview)
+    .put(ReviemsCtrl.apiUpdateReview)
+    .delete(ReviemsCtrl.apiDeleteReview)
 
 export default router
