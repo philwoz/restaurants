c6ba3b7097674a6c9cd74f225f4ff204a7251ddd
philwoz <61164019+philwoz@users.noreply.github.com>
2022-01-06 18:42:41 +0000

    created react app for frontend development

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -42,18 +42,18 @@ export default class RestaurantsController {
             res.json(restaurant)
         } catch (e) {
             console.log(`api, ${e}`)
-            res.status(500).json({ error: e})
+            res.status(500).json({ error: e })
         }
     }
 
     static async apiGetRestaurantCuisines(req, res, next) {
         try {
-          let cuisines = await RestaurantsDAO.getCuisines()
-          res.json(cuisines)
+            let cuisines = await RestaurantsDAO.getCuisines()
+            res.json(cuisines)
         } catch (e) {
-          console.log(`api, ${e}`)
-          res.status(500).json({ error: e })
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
         }
-      }
+    }
 
 }
