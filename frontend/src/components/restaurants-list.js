36fe3d9270ee3dd5dd516ca631c518668a6c400c
philwoz <61164019+philwoz@users.noreply.github.com>
2022-01-11 22:10:18 +0000

    added state varibles to restarants-list.js file

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import RestaurantDataService from "../services/restaurant.js":
+import { link } from "react-router-dom";
 
 const RestaurantsList = props => {
+const [restaurants, setRestaurants] = useState([]);
+const [searchName, setSearchName] = useState("");
+const [searchZip, setSearchZip] = useState("");
+const [searchCuisine, setSearchCuisine] = useState("");
+const [cuisines, setCuisine] = useState("All Cuisines");
+
   return (
     <div className="App">
       Hello World
